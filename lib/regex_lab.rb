f35c749da2e45151cbad07da61fd01d980554bb7
Travis Clarkson <tk1667@gmail.com>
2018-02-23 03:28:39 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -3,7 +3,7 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  /\Aun..ing\b/.match(text).captures
+  /\Aun..ing\b/.scan(text)
 end
 
 def words_five_letters_long(text)
